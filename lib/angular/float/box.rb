cb6829e2e0ba5c5ba83fb4560f1ea581da335b2a
Alexander Mcritchie <alexandermcritchie@AlexMcRitchies-MacBook-Pro.local>
2017-06-14 10:25:15 -0600

    first commit

---
+++ lib/angular/float/box.rb
@@ -0,0 +1,9 @@
+require "angular/float/box/version"
+
+module Angular
+  module Float
+    module Box
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/angular/float/box/version.rb
@@ -0,0 +1,7 @@
+module Angular
+  module Float
+    module Box
+      VERSION = "0.1.0"
+    end
+  end
+end
