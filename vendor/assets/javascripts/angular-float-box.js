33478afd1e882d95a299dcf9c6d4e44a928775c9
Alexander Mcritchie <alexandermcritchie@AlexMcRitchies-MacBook-Pro.local>
2017-07-25 08:45:51 -0600

    Version 1.3

--- vendor/assets/javascripts/angular-float-box.js
+++ vendor/assets/javascripts/angular-float-box.js
@@ -6,66 +6,75 @@
 */
 
 (function (window, angular, undefined) {
-  /*jshint globalstrict:true*/
-  /*global angular:false*/
   'use strict';
-
   angular.module('angular-float-box', [])
-
-  .service('floatBox', ['$timeout',
-  function($timeout) {
-
-    var direction = 'right'
+  .service('floatBox', ['scroll', 'draggable',
+  function(scroll, draggable) {
 
     // Call floatBox.initializes in the controller to create listeners for scrolling and dragging float-box
-    this.initialize = function($scope, slide_direction) {
-      initialize_scroll($scope)
-      initialize_draggable($scope)
-      direction = slide_direction;
+    this.initialize = function($scope) {
+      scroll.initialize();
+      draggable.initialize($scope);
     }
 
-    // Initializes float-box's scroll listener.
-    function initialize_scroll($scope) {
-      // How many pixels needed for scroll to shrink.
-      var shrink_treshold = 10;
-      // Variable needed to check direction of scroll.
-      var last_scroll_top = 0;
-      // Boolean to mark if a shrink is still in process or not.
-      var animation_complete = true;
-      // Setup float-box scroll listener.
-      $('#float-box-container').scroll(function(event){
-        var scroll_top = $(this).scrollTop();
-        var is_down_scroll = (scroll_top > last_scroll_top)
-        // Check direction of scroll.
-        if (scroll_top > last_scroll_top){
-          // Down scroll
-          if (this.scrollTop > shrink_treshold) {
-            // Within threshold for shrinking.
-            if (animation_complete && !$('.float-box-head').hasClass("smaller")) {
-              $('.float-box-head').addClass("smaller");
-              $('.float-box-space').addClass("smaller");
-              animation_complete = false
-              setTimeout(function(){ animation_complete = true }, 500);
-            }
-          }
-        } else {
-          // Up scroll
-          if (this.scrollTop <= shrink_treshold) {
-            // Outside of threshold for shrinking.
-            if (animation_complete) {
-              $('.float-box-head').removeClass("smaller");
-              $('.float-box-space').removeClass("smaller");
-              animation_complete = false
-              setTimeout(function(){ animation_complete = true; }, 500);
-            }
-          }
-        }
-        last_scroll_top = scroll_top;
-      });
-    };
+    // Show float-box
+    this.show = function() {
+      $( "#float-box" ).show()
+    }
+    // Hide float-box
+    this.hide = function() {
+      $( "#float-box" ).hide()
+    }
+    // Show float-box via fade.
+    this.fade_in = function() {
+      $( "#float-box" ).show( "fade")
+    }
+    // Show float-box via fade.
+    function fade_in() {
+      $( "#float-box" ).show( "fade")
+    }
+    this.fade_in = fade_in;
+    // Hide float-box via fade.
+    this.fade_out = function() {
+      if ($(window).width() < 768) {
+        $( "#float-box" ).hide( "fade")
+      };
+    }
 
-    // Initializes float-box's positionable listener.
-    function initialize_draggable($scope) {
+    // Display float-box-create tab.
+    this.create = function() {
+      // Show float-box-create-tab if not already shown.
+      $('float-box-create-tab a').tab('show');
+      // Reset defaults and show if needed.
+      new_float_box_instance($('float-box-create'));
+    }
+    // Display float-box-read tab.
+    this.read = function() {
+      // Show float-box-read-tab if not already shown.
+      $('float-box-read-tab a').tab('show');
+      // Reset defaults and show if needed.
+      new_float_box_instance($('float-box-read'));
+    }
+    // Display float-box-update tab.
+    this.update = function() {
+      // Show float-box-update-tab if not already shown.
+      $('float-box-update-tab a').tab('show')
+      // Reset defaults and show if needed.
+      new_float_box_instance($('float-box-update'))
+    }
+
+    // function refresh_float_box($action_tab) {
+    function new_float_box_instance($action_tab) {
+      // Show float-box if needed.
+      fade_in()
+      // Show default tab.
+      $action_tab.find('.float-box-default-tab').tab('show')
+      // Focus on default input.
+      $action_tab.find('.float-box-default-input').focus()
+    }
+  }])
+  .service('draggable', [function() {
+    this.initialize = function($scope) {
       $scope.draggable_on = function() {
         $('#float-box').draggable('enable');
         $scope.draggable_disabled = false;
@@ -77,100 +86,121 @@
       }
       $scope.draggable_disabled = true;
       $('#float-box').draggable().draggable('disable');
-    };
-
-    // Show float-box
-    this.show = function() {
-      $( "#float-box" ).show()
-    }
-    // Hide float-box
-    this.hide = function() {
-      $( "#float-box" ).hide()
-    }
-    // Show float-box via slide in from the direction specified.
-    this.slide_in = function(direction) {
-      $( "#float-box" ).show( "drop", {direction: direction})
-    }
-    // Show float-box via slide in from the direction specified.
-    function slide_in(slide_direction) {
-      direction = slide_direction || direction
-      $( "#float-box" ).show( "drop", {direction: direction})
-    }
-    this.slide_in = slide_in;
-    // Hide float-box via slide in from the direction specified.
-    this.slide_out = function(direction) {
-      if ($(window).width() < 768) {
-        $( "#float-box" ).hide( "drop", {direction: direction})
-      };
-    }
-
-    // Display float-box-create tab.
-    this.create = function() {
-      // Show float-box-create-tab if not already shown.
-      $('#float-box-create-tab').tab('show');
-      // Reset defaults and show if needed.
-      new_float_box_instance($('float-box-create'));
-    }
-
-    // Display float-box-read tab.
-    this.read = function() {
-      // Show float-box-read-tab if not already shown.
-      $('#float-box-read-tab').tab('show');
-      // Reset defaults and show if needed.
-      new_float_box_instance($('float-box-read'));
-    }
-
-    // Display float-box-update tab.
-    this.update = function() {
-      // Show float-box-update-tab if not already shown.
-      $('#float-box-update-tab').tab('show')
-      // Reset defaults and show if needed.
-      new_float_box_instance($('float-box-update'))
-    }
-
-    // function refresh_float_box($action_tab) {
-    function new_float_box_instance($action_tab) {
-      // Show float-box if needed.
-      slide_in(direction)
-      // Show default tab.
-      $action_tab.find('.float-box-default-tab').tab('show')
-      // Focus on default input.
-      $action_tab.find('.float-box-default-input').focus()
     }
   }])
-
-  .directive('floatBoxTitle', ['$window', function ( $window ) {
+  .service('scroll', ['$timeout', 'title',
+  function($timeout, title) {
+    this.initialize = function() {
+      // How many pixels needed for scroll to shrink.
+      var shrink_treshold = 10;
+      // Variable needed to check direction of scroll.
+      var last_scroll_top = 0;
+      // Boolean to mark if a shrink is still in process or not.
+      var animation_complete = true;
+      // Setup float-box scroll listener.
+      $('.float-box-container').scroll(function(event){
+        // Distance from top of float-box-container
+        var scroll_top = $(this).scrollTop();
+        // Check direction of scroll.
+        if (scroll_top > last_scroll_top){
+          // Down scroll
+          if (this.scrollTop > shrink_treshold) {
+            // Within threshold for shrinking.
+            if (animation_complete && !$('.float-box-container').hasClass("smaller")) {
+              $('.float-box-container').addClass("smaller");
+              animation_complete = false
+              $timeout(function(){ animation_complete = true }, 500);
+            }
+          }
+        } else {
+          // Up scroll
+          if (this.scrollTop <= shrink_treshold) {
+            // Outside of threshold for shrinking.
+            if (animation_complete) {
+              $('.float-box-container').removeClass("smaller");
+              // Adjust title when smaller class is removed.
+              title.adjust($('float-box-title:visible'))
+              animation_complete = false
+              $timeout(function(){ animation_complete = true; }, 500);
+            }
+          }
+        }
+        last_scroll_top = scroll_top;
+      });
+    };
+  }])
+  .service('title', [function() {
+    var max_font_size = 40;
+    this.max_font_size = max_font_size;
+    // Adjust font-size of title based on size of text.
+    this.adjust = function($title) {
+      var $text = $title.find('.float-box-title-text');
+      // Add notransition so no transition animation will get in the way of evaluating the proper font-size of title
+      $text.addClass('notransition')
+      // Initialize font-size of $text.
+      var font_size = max_font_size;
+      // Update font-size of $text.
+      $text.css('font-size', font_size+'px');
+      // Lower font-size until $text is thinner than $title or font-size is 20.
+      while (($title.width() < $text.width()) && font_size != 20) {
+        // Lower font-size by 5.
+        font_size -= 5;
+        // Update font-size of $text.
+        $text.css('font-size', font_size+'px');
+      }
+      // Turn on css transitions now that font-size is updated.
+      $text.removeClass('notransition')
+    }
+  }])
+  // Float Box Title.
+  .directive('floatBoxTitle', ['title', function (title ) {
     "use strict";
     return {
       restrict: 'E',
       template: '<div class="float-box-title-text">{{text}}</div>',
-      scope: {text: '='},
+      scope: {
+        text: '='
+      },
       link: function($scope, $title, iAttrs) {
+        // Watch for float-box-title element appearing.
+        $scope.$watch(function() { return angular.element($title).is(':visible') }, function() {
+          title.adjust($title) // Adjust font-size of title.
+        });
+        // Watch for changes in float-box-title text.
         $scope.$watch('text', function(a,b,c) {
-          var $text = $title.find('.float-box-title-text');
-          // Turn off css transitions so while loop doesn't get stuck.
-          $text.addClass('notransition')
-          // Initialize font-size of title.
-          var font_size = 40
-          $text.css('font-size', font_size+'px');
-          // Lower font-size until $text is thinner than $title or font-size is 20.
-          while (($title.width() < $text.width()) && font_size != 20) {
-            // Lower font-size by 5 and update font-size of $text.
-            font_size -= 5
-            $text.css('font-size', font_size+'px');
-          }
-          // Turn on css transitions now that font-size is updated.
-          $text.removeClass('notransition')
+          title.adjust($title) // Adjust font-size of title.
         })
       }
     }
   }])
-
-  .directive('floatBoxSearchBar', ['$window', function ( $window ) {
+  // Float Box Limit.
+  .directive('floatBoxLimit', [function () {
     "use strict";
     return {
       restrict: 'E',
-      template: '<div class="float-box-title-text">{{text}}</div>',
+      template: '{{display}}',
+      scope: {
+        text: '=',
+        limit: '='
+      },
+      link: function($scope, $title, iAttrs) {
+        $scope.$watch('text', function(a,b,c) {
+          // Duplicate the text that will be limited as the variable display.
+          $scope.display = angular.copy($scope.text)
+          // Check if the limited text is longer than the limit.
+          if ($scope.text && $scope.text.length >= $scope.limit) {
+            // If the limited text is longer than the limit, truncate the display.
+            $scope.display = $scope.display.substring(0,$scope.limit - 1) + '..'
+          }
+        })
+      }
+    }
+  }])
+  // Float Box Search Bar.
+  .directive('floatBoxSearchBar', [function () {
+    "use strict";
+    return {
+      restrict: 'E',
       template:
       '<span class="input-group-btn">' +
       '<button class="btn btn-info btn-sm" type="submit">' +
@@ -180,6 +210,16 @@
       '<input type="text" class="form-control input-sm" placeholder="{{placeholder}}" ng-model="model" autofocus>',
       scope: {model: '=', placeholder: '='}
     }
+  }])
+  // Float Box Tabs
+  .directive('floatBoxCreateTab', [function () {
+    return { template: '<a data-toggle="tab" data-target="float-box-create"></a>' };
+  }])
+  .directive('floatBoxReadTab', [function () {
+    return { template: '<a data-toggle="tab" data-target="float-box-read"></a>' };
+  }])
+  .directive('floatBoxUpdateTab', [function () {
+    return { template: '<a data-toggle="tab" data-target="float-box-update"></a>' };
   }]);
 
 })(window, window.angular);
